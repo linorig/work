723968da43420fb1ec1d0d4ae65018d8e77d19a5
Alecsandr <linorig@yandex.ru>
2021-03-14 19:24:05 +0700

    Создали новое приложение

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+"use strict";
+const numberOfFilms =+ prompt('сколько фильмов вы уже просмотрели','');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Cколько фильмов вы уже просмотрели',''), 
+      b = prompt('Какая оценка фильма',''),
+      c = prompt('Cколько фильмов вы уже просмотрели',''),
+      d = prompt('Какая оценка фильма','');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = d;
+
+console.log(personalMovieDB);
