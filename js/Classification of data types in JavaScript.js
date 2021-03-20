886803e37daa384d43167e5078ceed1511707b63
Alecsandr <linorig@yandex.ru>
2021-03-21 00:03:07 +0700

    file new

---
+++ js/Classification of data types in JavaScript.js
@@ -0,0 +1,37 @@
+/*
+Простые типы это:
+1. Числа 1, 2, 3
+2. Строки 'string', 'name' это простые строки
+3. Логический тип (boolean) true/false
+4. null - эток когда просто ничего не существует
+5. undefined - например функция без дерективы return возвращает пустой обьект
+*/
+
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string'*9);
+
+const persone = `alex`;
+
+const bool = false;
+
+let und;
+console.log(und);
+
+const obj = {
+    name:"Flex",
+    age: 35,
+    isMarried:false
+};
+
+console.log(obj["name"]);
+
+let arr = ['plum.ping', 8, 'loopt.root'];
+
+console.log(arr[1]);
+
+
+
